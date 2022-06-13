b73aa4cf14c8507fc3aee248200cc08a8befc045
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-13 07:53:41 -0500

    added modal settings

--- src/main/resources/static/mainPage.js
+++ src/main/resources/static/mainPage.js
@@ -1,46 +1,43 @@
 'use strict';
 
 
+const btnAddArtwork = document.querySelector(".btnAddArtwork")
+
 const addNewArtwork = function(){
-    // const inputName = document.querySelector('#newArtworkName').value;
-    // const inputArtist = document.querySelector('#newArtworkArtist').value;
-    // const inputDate = document.querySelector('#newArtworkDate').value;
-    // const inputImage = document.querySelector('#newArtworkURL').value;
+    const inputName = document.querySelector('#newArtworkName').value;
+    const inputArtist = document.querySelector('#newArtworkArtist').value;
+    const inputDate = document.querySelector('#newArtworkDate').value;
+    const inputImage = document.querySelector('#newArtworkURL').value;
 
+    if(inputName === "" || inputArtist === "" || inputDate === "" || inputImage === ""){
+        alert("Please fill out all fields");
+    }
+    else{
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:8080/api/v1/artwork/");
+        xhr.setRequestHeader("Content-Type", "application/json");
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:8080/api/v1/artwork/");
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onload = function() {
-        if (xhr.status !== 200) { // analyze HTTP status of the response
-            alert(xhr.response); // e.g. 404: Not Found
-            xhr.onerror = () => console.log(`Loaded: ${xhr.status} ${xhr.response}`);
-        } else { // show the result
-            alert(`Artwork has been added!`); // response is the server response
-        }
-    };
-
-
-
-    let data = `{
-            "name":"${document.querySelector('#newArtworkName').value}",
-            "artist":"${document.querySelector('#newArtworkArtist').value}",
-            "date":"${document.querySelector('#newArtworkDate').value}",
-            "image":"${document.querySelector('#newArtworkURL').value}"
+        let data = `{
+            "name":"${inputName}",
+            "artist":"${inputArtist}",
+            "date":"${inputDate}",
+            "image":"${inputImage}"
         }`;
+        xhr.send(data);
 
-    xhr.send(data);
-    document.getElementById("addArtworkForm").reset();
-
-
-    var element = document.getElementById("successArtwork");
-    element.style.display = "block";
-
+        xhr.onload = function() {
+            if (xhr.status !== 200) { // analyze HTTP status of the response
+                alert("Artwork is already in database"); // e.g. 404: Not Found
+                xhr.onerror = () => console.log(`Loaded: ${xhr.status} ${xhr.response}`);
+            } else { // show the result
+                alert(`Artwork has been added!`); // response is the server response
+                document.getElementById("addArtworkForm").reset();
+            }
+        };
+    }
 }
 
-function changeStyle(){
-    var element = document.getElementById("myDiv");
-    //    element.style.display = "none";
+const closeModal = function (){
     window.location.reload();
 }
+
