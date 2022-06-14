3dad8a000f4abdcdddb59ebaf383a04daf159240
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-13 20:32:04 -0500

    adding more pieces

--- src/main/java/com/example/demo/Artwork/ArtworkConfig.java
+++ src/main/java/com/example/demo/Artwork/ArtworkConfig.java
@@ -11,21 +11,40 @@ public class ArtworkConfig {
     CommandLineRunner commandLineRunner(ArtworkRepository repository){
         return args -> {
             Artwork monaLisa = new Artwork(
-                    "Mona Lisa",
-                    1534,
-                    "Leonardo da Vinci",
-                    "https://upload.wikimedia.org/wikipedia/commons/6/6a/Mona_Lisa.jpg"
+                    "The Tower of Babel",
+                    1563,
+                    "Pieter Bruegel the Elder",
+                    "https://arthistoryproject.com/site/assets/files/9971/pieter_bruegel_the_elder_-_the_tower_of_babel_1563_114x155cm_kunsthistorisches_museum_vienna-1.jpg"
             );
 
+            Artwork nighthawks = new Artwork(
+                    "Nighthawks",
+                    1942,
+                    "Edward Hopper",
+                    "https://i.redd.it/1ofywg0vbrvz.jpg"
+            );
+
+
             Artwork selfPortrait = new Artwork(
-                    "Self-Portrait with Cropped Hair",
+                    "Edward Hopper",
                     1940,
                     "Frida Kahlo",
                     "https://www.moma.org/media/W1siZiIsIjQ5NDA4NSJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=8e5f2e8b5844fd7b"
             );
 
+            Artwork sleepingGypsy = new Artwork(
+                    "The Sleeping Gypsy ",
+                    1897,
+                    "Henri Rousseau",
+                    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fuploads8.wikiart.org%2Fimages%2Fhenri-rousseau%2Fthe-sleeping-gypsy-1897.jpg&f=1&nofb=1"
+            );
+
+
+
             repository.save(monaLisa);
+            repository.save(nighthawks);
             repository.save(selfPortrait);
+            repository.save(sleepingGypsy);
         };
     }
 }
