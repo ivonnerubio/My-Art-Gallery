ff8231e8b9c0066a8bcef652d444a8049374cdec
ivonnerubio <ivonnerubiouh@gmail.com>
2022-06-13 22:23:06 -0500

    changing mobile view

--- src/main/java/com/example/demo/Artwork/ArtworkConfig.java
+++ src/main/java/com/example/demo/Artwork/ArtworkConfig.java
@@ -26,7 +26,7 @@ public class ArtworkConfig {
 
 
             Artwork selfPortrait = new Artwork(
-                    "Edward Hopper",
+                    "Self-Portrait with Cropped Hair",
                     1940,
                     "Frida Kahlo",
                     "https://www.moma.org/media/W1siZiIsIjQ5NDA4NSJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=8e5f2e8b5844fd7b"
